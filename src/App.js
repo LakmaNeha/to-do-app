ae56a11fd1ea2c4f2f7cdb9299868b66fb9239eb
NehaLakma <lakma.neha@gmail.com>
2021-04-17 04:02:40 +0530

    docs: Added README.md

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { db } from "./firebase_config";
 import firebase from "firebase";
 import TodoListItem from "./Todo";
 
+var color="yellow";
+
+
 function App() {
   const [todos, setTodos ] = useState([]);
   const [todoInput, setTodoInput] = useState("");
@@ -27,11 +30,17 @@ function App() {
 
   function addTodo(event){
     event.preventDefault();
+
+    if(todoInput.trim().length===0){
+      alert(" Try to type something")
+    }
+    else{
     db.collection("todos").add({
       is_in_progress: true,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         todo: todoInput,
     });  
+    }
 
     setTodoInput("");
 
@@ -45,17 +54,17 @@ function App() {
     <div className="App" >
 
      <div style={{display:"flex",flexDirection:"column",justifyContent: "center",alignItems: "center", width: "100%" }}>
-     <h1>TO_DO </h1> 
+     <h1 style={{ backgroundColor: color }}>TO_DO </h1> 
 
     <form>
-
-    < TextField id="standard-basic" label="Write a to-do" value={todoInput} style={{width: "90vw", maxWidth: "500px"}} onChange={todoInputHandler} />
+    
+    < TextField id="outlined-basic" label="Write a to-do" variant="outlined" value={todoInput} style={{width: "90vw", maxWidth: "500px", backgroundColor: "white"}} onChange={todoInputHandler} />
 
     <Button type = "submit" variant="contained" onClick = {addTodo} style={{display: "none"}}>Add</Button>
 
     </form>
  
-    <div style={{width: "90vw", maxWidth: "500px" , marginTop: "24px"}} >
+    <div  style={{width: "90vw", maxWidth: "500px" , marginTop: "24px", }} >
     {todos.map((todo) => (
     <TodoListItem 
     todo={todo.todo}
---
+++ src/README.md
@@ -0,0 +1,3 @@
+# to-do App
+ It is used to log your daily tasks and delete  when you are done with a task.
+ Built using `ReactJS` and deployed using `Firebase`
